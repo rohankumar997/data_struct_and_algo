c82376b4e038024502c48dedbbf92115bca25235
AG16000 <rohankumar997>
2020-11-19 12:48:18 +0530

    ds algo updates

--- reverse_in_array.py
+++ reverse_in_array.py
@@ -1,12 +1,12 @@
-def reverse_arrar(arr,start,end):
+def reverse_array(arr,start,end):
     while start<end:
         arr[start],arr[end]=arr[end],arr[start]
         start+=1
         end-=1
 
 arr=[1,2,3,4,5,6]
-
+print("unreversed array: ")
 print(arr)
-
-reverse_arrar(arr,0,len(arr)-1)
+print("reversed array:")
+reverse_array(arr,0,len(arr)-1)
 print(arr)
